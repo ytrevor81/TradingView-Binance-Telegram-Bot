04809da1d4279185c374e2b39494bf7ed62be168
Trevor Youngquist <ytrevor81@gmail.com>
2021-04-02 12:48:26 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+SPOTTEST_API_KEY = "Spot Test API Key here (Replace with real Binance API key when you are finished with testing)"
+SPOTTEST_SECRET_KEY = "Spot Test Secret Key here (Replace with real Binance secret key when you are finished with testing)"
+PASSPHRASE = "Passphrase for TradingView Alerts (optional and easy to remove from your TradingView JSON alert)"
+TELEGRAM_BOT_KEY = "Telegram Bot key here"
